f0a41d6e4c73f0e43a49d07ab83274607ac4d281
Davidson Sousa <dev@davidsonsousa.net>
2019-03-04 17:14:59 +0100

    Removed Core project; Added new library

--- TimeTracker.Core/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TimeTracker.Core
-{
-    public class Class1
-    {
-    }
-}
---
+++ TimeTracker/Data/Models/BaseModel.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TimeTracker.Data.Models
+{
+    public class BaseModel
+    {
+        #region Not mapped
+        [NotMapped]
+        public bool IsNew
+        {
+            get
+            {
+                return (Id == 0 && VanityId == Guid.Empty);
+            }
+        }
+        #endregion
+
+        public bool IsDeleted { get; set; }
+
+        [Key]
+        public int Id { get; set; }
+
+        public Guid VanityId { get; set; }
+
+        #region For "quick log" purposes
+
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+
+        public string UserCreated { get; set; }
+        public string UserModified { get; set; }
+
+        #endregion
+
+        public override string ToString()
+        {
+            var jsonResult = new JObject(
+                                        new JProperty("Id", Id),
+                                        new JProperty("VanityId", VanityId)
+                                    );
+            return jsonResult.ToString();
+        }
+    }
+}
