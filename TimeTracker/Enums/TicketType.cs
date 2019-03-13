88048fc2447f2a6a91f65c89cd08686d61414a05
Davidson Sousa <dev@davidsonsousa.net>
2019-03-13 23:04:24 +0100

    Added enum for ticket type

---
+++ TimeTracker/Enums/TicketType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeTracker.Enums
+{
+    public enum TicketType
+    {
+        HomeOffice,
+        Vacation,
+        SickDay,
+        BusinessTrip
+    }
+}
