b1bdd05c90c572ab1d8e5f6fecc1b5588b27fa58
Davidson Sousa <dev@davidsonsousa.net>
2019-03-13 23:08:53 +0100

    Added Attendance model

---
+++ TimeTracker/Data/Models/Attendance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeTracker.Data.Models
+{
+    public class Attendance : BaseModel
+    {
+        #region Relationship
+
+        public virtual Calendar Calendar { get; set; }
+
+        #endregion
+
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+    }
+}
