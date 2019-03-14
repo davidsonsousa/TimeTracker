82776b8a54c6faed055af93171250fcd2d7c7c6b
Davidson Sousa <dev@davidsonsousa.net>
2019-03-14 22:08:42 +0100

    Updated Startup file with database and unit of work configuration

--- TimeTracker.Attendance/Startup.cs
+++ TimeTracker.Attendance/Startup.cs
@@ -6,21 +6,40 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using TimeTracker.Attendance.Components;
+using TimeTracker.Data;
 using TimeTracker.Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace TimeTracker.Attendance
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add data context
+            services.AddDbContextPool<TimeTrackerContext>(options =>
+                                                          options.UseLazyLoadingProxies()
+                                                                 .EnableSensitiveDataLogging(true) // TODO: Perhaps use a flag from appsettings instead of a hard-coded value
+                                                                 .UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            // Add Unit of Work
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
             services.AddMvc()
-                .AddNewtonsoftJson();
+                    .AddNewtonsoftJson();
 
             services.AddRazorComponents();
 
