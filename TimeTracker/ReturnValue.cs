f7e1574ca458d263126b3b5ffc97be11c8c6e4fd
Davidson Sousa <sousa@cngroup.dk>
2019-03-15 16:56:02 +0100

    Added return value object

---
+++ TimeTracker/ReturnValue.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeTracker
+{
+    public class ReturnValue
+    {
+        [JsonProperty(PropertyName = "value")]
+        public object Value { get; set; }
+
+        [JsonProperty(PropertyName = "message")]
+        public string Message { get; set; }
+
+        [JsonProperty(PropertyName = "exception")]
+        public string Exception { get; set; }
+
+        [JsonProperty(PropertyName = "isError")]
+        public bool IsError { get; set; }
+    }
+}
