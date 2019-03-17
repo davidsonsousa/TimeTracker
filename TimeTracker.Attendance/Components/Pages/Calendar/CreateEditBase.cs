0688f452bf7f9e32cfc6477da8eb0419cb52d46a
Davidson Sousa <dev@davidsonsousa.net>
2019-03-17 21:54:53 +0100

    Separated Calendar into smaller components

---
+++ TimeTracker.Attendance/Components/Pages/Calendar/CreateEditBase.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.Data.EditModels;
+using TimeTracker.Services;
+
+namespace TimeTracker.Attendance.Components.Pages.Calendar
+{
+    public class CreateEditBase : ComponentBase
+    {
+        protected CalendarEditModel calendarEditModel = new CalendarEditModel();
+
+        [Inject]
+        public Service Service { get; set; }
+
+        protected void AddCalendar()
+        {
+            var returnValue = Service.SaveCalendar(calendarEditModel);
+        }
+    }
+}
---
+++ TimeTracker.Attendance/Components/Pages/Calendar/ListBase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.Data.ViewModels;
+using TimeTracker.Services;
+
+namespace TimeTracker.Attendance.Components.Pages.Calendar
+{
+    public class ListBase : ComponentBase
+    {
+        protected IEnumerable<CalendarViewModel> calendars = new List<CalendarViewModel>();
+
+        [Inject]
+        public Service Service { get; set; }
+
+        protected override Task OnInitAsync()
+        {
+            RefreshList();
+
+            return base.OnInitAsync();
+        }
+
+        protected void RefreshList()
+        {
+            calendars = Service.GetCalendarsReadOnly();
+        }
+    }
+}
