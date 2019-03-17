26c51bfac1151b23cfe448d93986be7748ca185a
Davidson Sousa <dev@davidsonsousa.net>
2019-03-17 23:32:03 +0100

    Added alert component into create/edit

--- TimeTracker.Attendance/Components/Pages/Calendar/CreateEditBase.cs
+++ TimeTracker.Attendance/Components/Pages/Calendar/CreateEditBase.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TimeTracker.Attendance.Components.Shared;
 using TimeTracker.Data.EditModels;
 using TimeTracker.Services;
 
@@ -12,13 +13,23 @@ namespace TimeTracker.Attendance.Components.Pages.Calendar
     public class CreateEditBase : ComponentBase
     {
         protected CalendarEditModel calendarEditModel = new CalendarEditModel();
+        protected Alert messageAlert;
 
         [Inject]
         public Service Service { get; set; }
 
+        protected override Task OnInitAsync()
+        {
+            return base.OnInitAsync();
+        }
+
         protected void AddCalendar()
         {
             var returnValue = Service.SaveCalendar(calendarEditModel);
+
+            messageAlert.Message = returnValue.Message;
+            messageAlert.AlertType = returnValue.IsError ? "danger" : "success";
+            messageAlert.Show();
         }
     }
 }
