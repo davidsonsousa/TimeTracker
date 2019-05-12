4d4213cd35452627e226242071b253604572dafb
Davidson Sousa <dev@davidsonsousa.net>
2019-05-12 10:19:23 +0200

    Added pages for holidays

---
+++ TimeTracker.Attendance/Pages/Holiday/CreateEditBase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TimeTracker.Attendance.Shared;
+using TimeTracker.Data.EditModels;
+using TimeTracker.Services;
+
+namespace TimeTracker.Attendance.Pages.Holiday
+{
+    public class CreateEditBase : ComponentBase
+    {
+        protected HolidayEditModel holidayEditModel = new HolidayEditModel();
+        protected Alert messageAlert;
+
+        [Inject]
+        public Service Service { get; set; }
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+        }
+
+        protected void AddHoliday()
+        {
+            var returnValue = Service.SaveHoliday(holidayEditModel);
+
+            messageAlert.Message = returnValue.Message;
+            messageAlert.AlertType = returnValue.IsError ? "danger" : "success";
+            messageAlert.Show();
+        }
+    }
+}
---
+++ TimeTracker.Attendance/Pages/Holiday/ListBase.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TimeTracker.Data.ViewModels;
+using TimeTracker.Services;
+
+namespace TimeTracker.Attendance.Pages.Holiday
+{
+    public class ListBase : ComponentBase
+    {
+        protected IEnumerable<HolidayViewModel> holidays = new List<HolidayViewModel>();
+
+        [Inject]
+        protected Service Service { get; set; }
+
+        protected override Task OnInitAsync()
+        {
+            RefreshList();
+
+            return base.OnInitAsync();
+        }
+
+        protected void RefreshList()
+        {
+            holidays = Service.GetHolidaysReadOnly();
+        }
+    }
+}
