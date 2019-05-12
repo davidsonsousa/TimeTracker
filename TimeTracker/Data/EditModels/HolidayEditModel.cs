5e560a0b82778279302389d31e83e29dff6b6668
Davidson Sousa <dev@davidsonsousa.net>
2019-05-12 10:18:54 +0200

    Added models for holidays

---
+++ TimeTracker/Data/EditModels/HolidayEditModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeTracker.Data.EditModels
+{
+    public class HolidayEditModel : BaseEditModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
