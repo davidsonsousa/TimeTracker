7e5e048a6e67e0b354c66e7dfe9859544e1debaa
Davidson Sousa <davidson.sousa@gmail.com>
2019-09-02 22:38:53 +0200

    Small refactoring

--- TimeTracker.Data/Entities/Attendance.cs
+++ TimeTracker.Data/Entities/Attendance.cs
@@ -4,11 +4,7 @@ namespace TimeTracker.Data.Entities
 {
     public class Attendance : BaseModel
     {
-        #region Relationship
-
         public virtual Branch Calendar { get; set; }
-
-        #endregion
 
         public DateTime Date { get; set; }
         public DateTime StartTime { get; set; }
