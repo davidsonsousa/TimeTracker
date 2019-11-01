e0095b070891f812247e1e6f9a91c62e84bc8f71
Davidson Sousa <dev@davidsonsousa.net>
2019-11-01 21:48:03 +0100

    Added context

--- TimeTracker.Infrastructure/TimeTrackerContext.cs
+++ TimeTracker.Infrastructure/TimeTrackerContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TimeTracker.Infrastructure.Models;
 
 namespace TimeTracker.Infrastructure
 {
     public class TimeTrackerContext : DbContext
     {
+        public DbSet<Timesheet> Timesheets { get; set; }
+        public DbSet<TimesheetDetails> TimesheetDetails { get; set; }
+
         public TimeTrackerContext(DbContextOptions<TimeTrackerContext> options) : base(options)
         {
 
