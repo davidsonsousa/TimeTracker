3c90babacb70e9d9468fa19f286885192587c59f
Davidson Sousa <dev@davidsonsousa.net>
2019-11-01 21:47:37 +0100

    Added models

---
+++ TimeTracker.Infrastructure/Models/TimesheetDetails.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TimeTracker.Infrastructure.Models
+{
+    public class TimesheetDetails : BaseModel
+    {
+        public DateTime EntranceTime { get; set; }
+        public DateTime ExitTime { get; set; }
+    }
+}
