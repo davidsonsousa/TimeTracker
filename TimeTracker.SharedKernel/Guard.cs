d2536411c8ee97cea2fbfcffaee50d6bac7aa371
Davidson Sousa <dev@davidsonsousa.net>
2019-11-25 21:36:22 +0100

    Added extra guard for null or white space

--- TimeTracker.SharedKernel/Guard.cs
+++ TimeTracker.SharedKernel/Guard.cs
@@ -4,6 +4,11 @@ namespace TimeTracker.SharedKernel
 {
     public static class Guard
     {
+        /// <summary>
+        /// Throws exception when value is less or equal zero
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="parameterName"></param>
         public static void ForLessEqualZero(int value, string parameterName)
         {
             if (value <= 0)
@@ -12,6 +17,12 @@ namespace TimeTracker.SharedKernel
             }
         }
 
+        /// <summary>
+        /// Throws exception when value is greater or equal dateToPrecede
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="dateToPrecede"></param>
+        /// <param name="parameterName"></param>
         public static void ForPrecedesDate(DateTime value, DateTime dateToPrecede, string parameterName)
         {
             if (value >= dateToPrecede)
@@ -20,9 +31,27 @@ namespace TimeTracker.SharedKernel
             }
         }
 
+        /// <summary>
+        /// Throws exception when value is null or empty
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="parameterName"></param>
         public static void ForNullOrEmpty(string value, string parameterName)
         {
             if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentOutOfRangeException(parameterName);
+            }
+        }
+
+        /// <summary>
+        /// Throws exception when value is null or white space
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="parameterName"></param>
+        public static void ForNullOrWhiteSpace(string value, string parameterName)
+        {
+            if (string.IsNullOrWhiteSpace(value))
             {
                 throw new ArgumentOutOfRangeException(parameterName);
             }
