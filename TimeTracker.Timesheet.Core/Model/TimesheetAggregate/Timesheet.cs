35a90c736b4777053cb504d722e2d5a0c4efcb31
Davidson Sousa <dev@davidsonsousa.net>
2019-11-25 21:35:59 +0100

    Added Timesheet project

---
+++ TimeTracker.Timesheet.Core/Model/TimesheetAggregate/Timesheet.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TimeTracker.SharedKernel;
+
+namespace TimeTracker.Timesheet.Core.Model.TimesheetAggregate
+{
+    public class Timesheet : Entity<Guid>
+    {
+        public int BranchId { get; private set; }
+        public DateTime EventDate { get; private set; }
+
+        private List<TimesheetEvent> timeSheetEvents;
+
+        public IEnumerable<TimesheetEvent> TimesheetEvents
+        {
+            get
+            {
+                return timeSheetEvents.AsEnumerable();
+            }
+            private set
+            {
+                timeSheetEvents = (List<TimesheetEvent>)value;
+            }
+        }
+
+        public Timesheet(Guid id, DateTime eventDate, int branchId, IEnumerable<TimesheetEvent> timesheetEvents) : base(id)
+        {
+            EventDate = eventDate;
+            BranchId = branchId;
+            TimesheetEvents = timesheetEvents;
+        }
+
+        // For EF
+        public Timesheet() : base(Guid.NewGuid())
+        {
+
+        }
+    }
+}
---
+++ TimeTracker.Timesheet.Core/Model/TimesheetAggregate/TimesheetEvent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using TimeTracker.SharedKernel;
+
+namespace TimeTracker.Timesheet.Core.Model.TimesheetAggregate
+{
+    public class TimesheetEvent : Entity<Guid>
+    {
+        public int TimesheetId { get; private set; }
+        public int UserId { get; private set; }
+        public DateTime EventDate { get; private set; }
+
+        public TimesheetEvent(Guid id) : base(id)
+        {
+
+        }
+
+        // For EF
+        public TimesheetEvent() : base(Guid.NewGuid())
+        {
+
+        }
+
+        public static TimesheetEvent Create(int timesheetId, int userId, DateTime eventDate)
+        {
+            Guard.ForLessEqualZero(timesheetId, nameof(timesheetId));
+            Guard.ForLessEqualZero(userId, nameof(userId));
+
+            var timesheetEvent = new TimesheetEvent(Guid.NewGuid())
+            {
+                TimesheetId = timesheetId,
+                UserId = userId,
+                EventDate = eventDate
+            };
+
+            return timesheetEvent;
+        }
+    }
+}
