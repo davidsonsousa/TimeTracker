ad9d26a0e2b9fd932c41acc556f5a799b19c9de4
Davidson Sousa <dev@davidsonsousa.net>
2019-11-25 21:36:45 +0100

    Added attendance objects

--- TimeTracker.Attendance.Core/Model/AttendanceAggregate/Attendance.cs
+++ TimeTracker.Attendance.Core/Model/AttendanceAggregate/Attendance.cs
@@ -1,10 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using TimeTracker.SharedKernel;
+using TimeTracker.SharedKernel.ValueObjects;
 
 namespace TimeTracker.Attendance.Core.Model.AttendanceAggregate
 {
     public class Attendance : Entity<Guid>
     {
+        public int BranchId { get; private set; }
+        public DateTimeRange DateTimeRange { get; private set; }
 
+        private List<AttendanceEvent> _attendanceEvents;
+        public IEnumerable<AttendanceEvent> AttendanceEvent
+        {
+            get
+            {
+                return _attendanceEvents.AsEnumerable();
+            }
+            private set
+            {
+                _attendanceEvents = (List<AttendanceEvent>)value;
+            }
+        }
+
+        public Attendance(Guid id, DateTimeRange dateTimeRange, int branchId) : base(id)
+        {
+            DateTimeRange = dateTimeRange;
+            BranchId = branchId;
+            _attendanceEvents = new List<AttendanceEvent>();
+        }
+
+        // Constructor for EF
+        public Attendance() : base(Guid.NewGuid())
+        {
+
+        }
     }
 }
---
+++ TimeTracker.Attendance.Core/Model/AttendanceAggregate/AttendanceEvent.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TimeTracker.SharedKernel;
+using TimeTracker.SharedKernel.ValueObjects;
+
+namespace TimeTracker.Attendance.Core.Model.AttendanceAggregate
+{
+    public class AttendanceEvent : Entity<Guid>
+    {
+        public int AttendanceId { get; private set; }
+        public int UserId { get; private set; }
+        public int EventTypeId { get; private set; }
+        public DateTimeRange DateTimeRange { get; private set; }
+
+        public AttendanceEvent(Guid id) : base(id)
+        {
+
+        }
+
+        // For EF
+        public AttendanceEvent() : base(Guid.NewGuid())
+        {
+
+        }
+
+        public static AttendanceEvent Create(int attendanceId, int userId, int eventTypeId, DateTimeRange dateTimeRange)
+        {
+            Guard.ForLessEqualZero(attendanceId, nameof(attendanceId));
+            Guard.ForLessEqualZero(userId, nameof(userId));
+            Guard.ForLessEqualZero(eventTypeId, nameof(eventTypeId));
+
+            var attendanceEvent = new AttendanceEvent(Guid.NewGuid())
+            {
+                AttendanceId = attendanceId,
+                UserId = userId,
+                EventTypeId = eventTypeId,
+                DateTimeRange = dateTimeRange
+            };
+
+            return attendanceEvent;
+        }
+    }
+}
