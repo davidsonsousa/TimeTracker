c28bef7d210e82db8428cae275b539daf6988e03
Davidson Sousa <dev@davidsonsousa.net>
2019-12-01 15:40:27 +0100

    Added concrete repository and context class

---
+++ TimeTracker.CompanyManagement.Data/CrudContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TimeTracker.CompanyManagement.Core.Model;
+
+namespace TimeTracker.CompanyManagement.Data
+{
+    public class CrudContext : DbContext
+    {
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<Holiday> Holidays { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public CrudContext()
+        {
+
+        }
+    }
+}
---
+++ TimeTracker.CompanyManagement.Data/Repository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TimeTracker.CompanyManagement.Core.Interfaces;
+
+namespace TimeTracker.CompanyManagement.Data
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class, IEntity
+    {
+        private readonly CrudContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+        public Repository()
+        {
+
+        }
+        public Repository(CrudContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task<IEnumerable<TEntity>> ListAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task InsertAsync(TEntity entity)
+        {
+            await _dbSet.AddAsync(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entityToDelete = await _dbSet.FindAsync(id);
+            _dbSet.Remove(entityToDelete);
+        }
+
+        public async Task SaveAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
