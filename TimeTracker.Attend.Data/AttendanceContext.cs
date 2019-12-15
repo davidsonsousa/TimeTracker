5c3e3698a15213c57a450faf898e25d82adcfee1
Davidson Sousa <dev@davidsonsousa.net>
2019-12-15 19:37:43 +0100

    Added properties to AttendanceContext

--- TimeTracker.Attend.Data/AttendanceContext.cs
+++ TimeTracker.Attend.Data/AttendanceContext.cs
@@ -1,9 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TimeTracker.Attend.Core.Model.AttendanceAggregate;
 
 namespace TimeTracker.Attend.Data
 {
     public class AttendanceContext : DbContext
     {
+        public DbSet<Attendance> Attendances { get; set; }
+        public DbSet<AttendanceEvent> AttendanceEvents { get; set; }
+        public DbSet<AttendanceEventType> AttendanceEventType { get; set; }
+        public DbSet<Branch> Branches { get; set; }
+        public DbSet<Holiday> Holidays { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<User> Users { get; set; }
 
+        public AttendanceContext()
+        {
+
+        }
     }
 }
