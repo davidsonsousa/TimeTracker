64a25c4f8301375b6b084d7ffef34f1cf85a478b
Davidson Sousa <dev@davidsonsousa.net>
2019-12-25 17:41:34 +0100

    Refactored CrudContext by adding entity configuration

--- TimeTracker.CompanyManagement.Data/CrudContext.cs
+++ TimeTracker.CompanyManagement.Data/CrudContext.cs
@@ -1,21 +1,31 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using TimeTracker.CompanyManagement.Core.Model;
 
 namespace TimeTracker.CompanyManagement.Data
 {
-    public class CrudContext : DbContext
+    public class CrudContext : IdentityDbContext<User>
     {
         public DbSet<Branch> Branches { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<Holiday> Holidays { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<Team> Teams { get; set; }
-        public DbSet<User> Users { get; set; }
 
-        // TODO: Should I add the name of the connection here?
-        public CrudContext()
+        public CrudContext(DbContextOptions<CrudContext> options) : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Branch>(ModelConfiguration.ConfigureBranch);
+            builder.Entity<Company>(ModelConfiguration.ConfigureCompany);
+            builder.Entity<Holiday>(ModelConfiguration.ConfigureHoliday);
+            builder.Entity<Project>(ModelConfiguration.ConfigureProject);
+            builder.Entity<Team>(ModelConfiguration.ConfigureTeam);
+
+            base.OnModelCreating(builder);
         }
     }
 }
