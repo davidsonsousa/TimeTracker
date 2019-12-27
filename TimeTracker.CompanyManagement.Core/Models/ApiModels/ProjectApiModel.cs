92422b0714bb34391f3c5b8ab686f62288f20087
Davidson Sousa <dev@davidsonsousa.net>
2019-12-27 18:28:11 +0100

    Added project api model

---
+++ TimeTracker.CompanyManagement.Core/Models/ApiModels/ProjectApiModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TimeTracker.CompanyManagement.Core.Models.ApiModels
+{
+    public class ProjectApiModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
