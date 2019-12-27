710c720150fa457ab475cf0e967d311bf268c7d9
Davidson Sousa <dev@davidsonsousa.net>
2019-12-27 11:39:15 +0100

    Added TeamApiModel

---
+++ TimeTracker.CompanyManagement.Core/Models/ApiModels/TeamApiModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace TimeTracker.CompanyManagement.Core.Models.ApiModels
+{
+    public class TeamApiModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IList<int> Branches { get; set; }
+        public IList<int> Projects { get; set; }
+        public IList<int> TeamMembers { get; set; }
+    }
+}
