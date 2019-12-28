4cdc8d91b96c8244cd6e48edb1c7b44f965ec9c6
Davidson Sousa <dev@davidsonsousa.net>
2019-12-28 04:40:10 +0100

    Added HolidayApiModel

---
+++ TimeTracker.CompanyManagement.Core/Models/ApiModels/HolidayApiModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeTracker.CompanyManagement.Core.Models.ApiModels
+{
+    public class HolidayApiModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int BranchId { get; set; }
+    }
+}
