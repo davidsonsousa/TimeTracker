985693069d25ae03b599a06da8a9c9c46a9fc462
Davidson Sousa <dev@davidsonsousa.net>
2019-12-28 04:40:01 +0100

    Refactored Holiday model by adding BranchId

--- TimeTracker.CompanyManagement.Core/Models/Holiday.cs
+++ TimeTracker.CompanyManagement.Core/Models/Holiday.cs
@@ -8,5 +8,6 @@ namespace TimeTracker.CompanyManagement.Core.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public DateTime Date { get; set; }
+        public int BranchId { get; set; }
     }
 }
